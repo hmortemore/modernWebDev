65eba2f6e866153e6cea17661ae069b3f91fa368
Anthony Giuliano <agiulian@nd.edu>
2020-04-23 16:45:35 -0400

    reformatted get request on frontend

--- frontend/src/app/components/view-parties/parties-list/parties-list.controller.js
+++ frontend/src/app/components/view-parties/parties-list/parties-list.controller.js
@@ -1,4 +1,4 @@
-function PartiesListController(PartyModel) {
+function PartiesListController(PartyModel, $http) {
   var ctrl = this;
 
   ctrl.$onInit = function(){
@@ -6,34 +6,24 @@ function PartiesListController(PartyModel) {
       ctrl.parties = results;
     });
 
-    let xhr = new XMLHttpRequest();
+    $http.get('http://localhost:8080/client_token').then(function(response) {
+      console.log(response.data);
+      const CLIENT_TOKEN_FROM_SERVER = response.data;
+      var button = document.querySelector('#submit-button');
 
-    xhr.open('GET', 'http://localhost:8080/client_token', true);
+      braintree.dropin.create({
+        authorization: CLIENT_TOKEN_FROM_SERVER,
+        selector: '#dropin-container'
+      }, function (err, instance) {
+        button.addEventListener('click', function () {
+          instance.requestPaymentMethod(function (err, payload) {
+            // send payload.nonce to your server
+            console.log('Nonce: ', payload.nonce);
+          });
+        })
+      });
+    });
 
-    xhr.send();
-
-    xhr.onload = function() {
-      if (xhr.status != 200) { 
-        alert(`Error ${xhr.status}: ${xhr.statusText}`);
-      } else {
-        console.log('Response: ', xhr.response);
-        const CLIENT_TOKEN_FROM_SERVER = xhr.response;
-
-        var button = document.querySelector('#submit-button');
-
-        braintree.dropin.create({
-          authorization: CLIENT_TOKEN_FROM_SERVER,
-          selector: '#dropin-container'
-        }, function (err, instance) {
-          button.addEventListener('click', function () {
-            instance.requestPaymentMethod(function (err, payload) {
-              // send payload.nonce to your server
-              console.log('Nonce: ', payload.nonce);
-            });
-          })
-        });
-      }
-    };
   }
 }
 
