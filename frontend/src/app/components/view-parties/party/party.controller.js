f6fcbb854ae0a3867aa9d8d161cc3bbfe11a5944
Zachary Spitzer <zspitzer@nd.edu>
2020-04-27 21:21:34 -0400

    Party Card fixes

--- frontend/src/app/components/view-parties/party/party.controller.js
+++ frontend/src/app/components/view-parties/party/party.controller.js
@@ -6,6 +6,16 @@ function PartyController($scope) {
     document.getElementsByClassName('page-mask')[0].classList.remove('hidden');
     $scope.$emit('buyTicket', this.party.attributes);
   } 
+
+  this.getTimeFormat = function (partyTime){
+    var hour = (partyTime.getHours()+4) % 12;
+    var minutes = partyTime.getMinutes() < 10
+      ? "0"+partyTime.getMinutes()
+      : partyTime.getMinutes();
+    return hour + ":" + minutes + (partyTime.getHours()+4 > 12
+      ? " P.M."
+      : " A.M.");
+  }
 }
 
 angular
